4c798ddb3bbade91c834183434c6a441bce83ac3
Blair Anderson <blairanderson95@me.com>
2021-01-25 16:42:04 +0000

    Star colour.

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -47,10 +47,10 @@ const StyledFavouriteButton = styled('button')`
 
 	svg {
 		height: 24px;
-		fill: ${(props) => (props.isFavouriteMovie ? colours.gold : 'grey')};
+		fill: ${(props) => (props.isFavouriteMovie ? colours.gold : 'hsl(0, 0%, 25%)')};
 		margin-bottom: 16px;
 		margin-right: 8px;
-		stroke: ${(props) => (props.isFavouriteMovie ? colours.gold : 'grey')};
+		stroke: ${(props) => (props.isFavouriteMovie ? colours.gold : 'hsl(0, 0%, 25%)')};
 		stroke-width: 16px;
 		width: 24px;
 	}
